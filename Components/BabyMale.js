9066a5b6c770736ce900620cd94cf3ccfbb59ef2
Lars <larsf2005@gmail.com>
2020-05-24 01:35:41 +0100

    Tidy

---
+++ Components/BabyMale.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import { Text, View } from 'react-native'
+
+function BabyMale(props) {
+
+    // const [page, setPage] = useState(0);
+
+    // useEffect(() => {
+    // }, []);
+
+    return (
+        <View>
+            <Text>A fresh new Component. Make something nice of it.</Text>
+        </View>
+    )
+}
+
+export { BabyMale }
